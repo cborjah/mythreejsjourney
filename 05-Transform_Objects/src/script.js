2df64f80d9b80e93d2d2301c9d3615c11ff92eb8
Christian Borja <cpborja32@gmail.com>
2023-08-03 14:27:45 -0700

    completes lesson 5

--- 05-Transform_Objects/src/script.js
+++ 05-Transform_Objects/src/script.js
@@ -1,18 +1,42 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: "blue" })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: "red" })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: "green" })
+);
+cube3.position.x = 2;
+group.add(cube3);
+
+group.position.y = 1;
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(); // Can take a param to increase the size of the lines.
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -20,20 +44,20 @@ scene.add(mesh)
 const sizes = {
     width: 800,
     height: 600
-}
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
