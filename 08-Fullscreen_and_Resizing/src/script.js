0d864a80ebc9b1a029b5456ad70ce2b76335942b
Christian Borja <cpborja32@gmail.com>
2023-08-16 15:17:59 -0700

    completes lesson 8

--- 08-Fullscreen_and_Resizing/src/script.js
+++ 08-Fullscreen_and_Resizing/src/script.js
@@ -36,14 +36,38 @@ window.addEventListener("resize", () => {
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
 
-    // ! Make sure to update the renderer...
-    renderer.setSize(sizes.width, sizes.height);
-
     // Update camera
     camera.aspect = sizes.width / sizes.height;
 
     // ! When changing properties like aspect, you need to call the following...
     camera.updateProjectionMatrix();
+
+    // Update renderer
+    // ! Make sure to update the renderer...
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // Locks pixel ratio.
+});
+
+// Handles toggling fullscreen
+window.addEventListener("dblclick", () => {
+    // * The following makes this feature compatible for most browsers.
+    const fullscreenElement =
+        document.fullscreenElement || document.webkitFullscreenElement;
+
+    // * Note that one uses the canvas whereas the other uses document!
+    if (!fullscreenElement) {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen();
+        } else if (canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
 });
 
 /**
@@ -70,6 +94,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // Locks pixel ratio.
 
 /**
  * Animate
