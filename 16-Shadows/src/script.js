ae6a6eaff3dc073358d6a513731425203962dec7
Christian Borja <cpborja32@gmail.com>
2023-10-02 16:36:31 -0700

    perf: algorithm applied to shadow map

--- 16-Shadows/src/script.js
+++ 16-Shadows/src/script.js
@@ -44,7 +44,7 @@ directionalLight.shadow.camera.bottom = 2;
 directionalLight.shadow.camera.left = 2;
 directionalLight.shadow.camera.near = 1;
 directionalLight.shadow.camera.far = 6;
-directionalLight.shadow.radius = 10;
+// directionalLight.shadow.radius = 10; // * Does not work with PCFSoftShadowMap type
 
 /**
  * Camera Helper
@@ -133,6 +133,7 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 /**
  * Animate
