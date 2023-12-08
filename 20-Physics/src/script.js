e0cef56e96ee50a23a916c545c590520524f64ea
Christian Borja <cpborja32@gmail.com>
2023-12-08 14:43:58 -0800

    feat: add create sphere functiont to debug gui

--- 20-Physics/src/script.js
+++ 20-Physics/src/script.js
@@ -16,6 +16,16 @@ import CANNON from "cannon";
  * Debug
  */
 const gui = new GUI();
+const debugObject = {};
+
+debugObject.createSphere = () => {
+    createSphere(Math.random() * 0.5, {
+        x: Math.random() - 0.5 * 3,
+        y: 3,
+        z: Math.random() - 0.5 * 3
+    });
+};
+gui.add(debugObject, "createSphere");
 
 /**
  * Base
