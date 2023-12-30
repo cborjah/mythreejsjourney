7d2afacb55a82e66cb9235ae50f5cfb047f2f14b
Christian Borja <cpborja32@gmail.com>
2023-12-29 22:17:39 -0800

    feat: add tone mapping

--- 25-Realistic_Render/src/script.js
+++ 25-Realistic_Render/src/script.js
@@ -1,77 +1,76 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import GUI from 'lil-gui'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+
+/**
+ * Tone Mapping
+ *
+ * Intends to convert High Dynamic Range (HDR) values to Low Dynamic Range (LDR) values.
+ * Three.js actually fakes the process of converting LDR to HDR even if the colors aren't
+ * HDR resulting in a very realistic render.
+ */
 
 /**
  * Loaders
  */
-const gltfLoader = new GLTFLoader()
-const rgbeLoader = new RGBELoader()
+const gltfLoader = new GLTFLoader();
+const rgbeLoader = new RGBELoader();
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI()
-const global = {}
+const gui = new GUI();
+const global = {};
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Update all materials
  */
-const updateAllMaterials = () =>
-{
-    scene.traverse((child) =>
-    {
-        if(child.isMesh && child.material.isMeshStandardMaterial)
-        {
-            child.material.envMapIntensity = global.envMapIntensity
+const updateAllMaterials = () => {
+    scene.traverse(child => {
+        if (child.isMesh && child.material.isMeshStandardMaterial) {
+            child.material.envMapIntensity = global.envMapIntensity;
         }
-    })
-}
+    });
+};
 
 /**
  * Environment map
  */
 // Global intensity
-global.envMapIntensity = 1
-gui
-    .add(global, 'envMapIntensity')
+global.envMapIntensity = 1;
+gui.add(global, "envMapIntensity")
     .min(0)
     .max(10)
     .step(0.001)
-    .onChange(updateAllMaterials)
+    .onChange(updateAllMaterials);
 
 // HDR (RGBE) equirectangular
-rgbeLoader.load('/environmentMaps/0/2k.hdr', (environmentMap) =>
-{
-    environmentMap.mapping = THREE.EquirectangularReflectionMapping
+rgbeLoader.load("/environmentMaps/0/2k.hdr", environmentMap => {
+    environmentMap.mapping = THREE.EquirectangularReflectionMapping;
 
-    scene.background = environmentMap
-    scene.environment = environmentMap
-})
+    scene.background = environmentMap;
+    scene.environment = environmentMap;
+});
 
 /**
  * Models
  */
 // Helmet
-gltfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
-    (gltf) =>
-    {
-        gltf.scene.scale.set(10, 10, 10)
-        scene.add(gltf.scene)
+gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", gltf => {
+    gltf.scene.scale.set(10, 10, 10);
+    scene.add(gltf.scene);
 
-        updateAllMaterials()
-    }
-)
+    updateAllMaterials();
+});
 
 /**
  * Sizes
@@ -79,58 +78,74 @@ gltfLoader.load(
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
-}
+};
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener("resize", () => {
     // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
     // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
     // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 5, 4)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+camera.position.set(4, 5, 4);
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.target.y = 3.5
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.target.y = 3.5;
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Tone mapping
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 3;
+
+gui.add(renderer, "toneMapping", {
+    No: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping
+});
+gui.add(renderer, "toneMappingExposure").min(0).max(10).step(0.001);
 
 /**
  * Animate
  */
-const tick = () =>
-{
+const tick = () => {
     // Update controls
-    controls.update()
+    controls.update();
 
     // Render
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+    window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
