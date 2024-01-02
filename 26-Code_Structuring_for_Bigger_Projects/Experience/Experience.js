0f6494a6eedde251994a41543eaa77f476d306d9
Christian Borja <cpborja32@gmail.com>
2024-01-02 13:19:37 -0800

    feat: add Camera class

--- 26-Code_Structuring_for_Bigger_Projects/Experience/Experience.js
+++ 26-Code_Structuring_for_Bigger_Projects/Experience/Experience.js
@@ -1,14 +1,29 @@
+import * as THREE from "three";
 import Sizes from "./Utils/Sizes.js";
 import Time from "./Utils/Time.js";
+import Camera from "./Camera.js";
+
+let instance = null;
 
 export default class Experience {
     constructor(canvas) {
+        if (instance) {
+            return instance;
+        }
+
+        instance = this;
+
+        // Global access
+        // window.experience = this;
+
         // Options
         this.canvas = canvas;
 
         // Setup
         this.sizes = new Sizes();
         this.time = new Time();
+        this.scene = new THREE.Scene();
+        this.camera = new Camera();
 
         // Sizes resize event
         this.sizes.on("resize", () => {
@@ -21,7 +36,11 @@ export default class Experience {
         });
     }
 
-    resize() {}
+    resize() {
+        this.camera.resize();
+    }
 
-    update() {}
+    update() {
+        this.camera.update();
+    }
 }
