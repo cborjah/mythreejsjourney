a08852cd54044e13e593c953b3e8923c69e89e88
Christian Borja <cpborja32@gmail.com>
2024-03-28 12:29:27 -0700

    build: add glsl plugin config to vite

--- 27-Shaders/vite.config.js
+++ 27-Shaders/vite.config.js
@@ -1,16 +1,19 @@
+import glsl from "vite-plugin-glsl";
+
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
+    root: "src/",
+    publicDir: "../static/",
+    base: "./",
+    server: {
         host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+        open: !(
+            "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env
+        ) // Open if it's not a CodeSandbox
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
+    build: {
+        outDir: "../dist", // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
-}
+    plugins: [glsl()]
+};
