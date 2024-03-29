61bbceb49cf92e4a616fea7e52a8c0931f8b4495
Christian Borja <cpborja32@gmail.com>
2024-03-28 22:33:11 -0700

    feat: add uniforms to vertex shader and ability to tweak their values

--- 27-Shaders/src/script.js
+++ 27-Shaders/src/script.js
@@ -41,10 +41,24 @@ geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1)); // The 
 // const material = new THREE.MeshBasicMaterial()
 const material = new THREE.RawShaderMaterial({
     vertexShader: testVertexShader,
-    fragmentShader: testFragmentShader
+    fragmentShader: testFragmentShader,
+    uniforms: {
+        uFrequency: { value: new THREE.Vector2(10, 5) }
+    }
     // wireframe: true,
     // side: THREE.DoubleSide // Render both sides
 });
+
+gui.add(material.uniforms.uFrequency.value, "x")
+    .min(0)
+    .max(20)
+    .step(0.01)
+    .name("frequencyX");
+gui.add(material.uniforms.uFrequency.value, "y")
+    .min(0)
+    .max(20)
+    .step(0.01)
+    .name("frequencyY");
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
--- 27-Shaders/src/shaders/test/vertex.vert
+++ 27-Shaders/src/shaders/test/vertex.vert
@@ -1,6 +1,7 @@
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+uniform vec2 uFrequency;
 
 // Retrieves the value of each vertex
 // Contains the x, y, and z coordinates from the attribute
@@ -17,6 +18,9 @@ void main()
    // Shorthand way of defining gl_Position
    // Same results, but this allows you to play with the model position
    vec4 modelPosition = modelMatrix * vec4(position, 1.0);   
+   modelPosition.z += sin(modelPosition.x * uFrequency.x) * 0.1;
+   modelPosition.z += sin(modelPosition.y * uFrequency.y) * 0.1;
+
    vec4 viewPosition = viewMatrix * modelPosition;
    vec4 projectedPosition = projectionMatrix * viewPosition;
 
