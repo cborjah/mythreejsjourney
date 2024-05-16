e72079e7aaf48e8f6116da6bf34a42721e2f8b9b
Christian Borja <cpborja32@gmail.com>
2024-05-16 16:50:03 -0700

    feat: add wave shapes

--- 29-Raging_Sea/src/script.js
+++ 29-Raging_Sea/src/script.js
@@ -25,8 +25,31 @@ const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128);
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
     vertexShader: waterVertexShader,
-    fragmentShader: waterFragmentShader
+    fragmentShader: waterFragmentShader,
+    uniforms: {
+        uBigWavesElevation: { value: 0.2 },
+        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) }
+    }
 });
+
+// Debug
+gui.add(waterMaterial.uniforms.uBigWavesElevation, "value")
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name("uBigWavesElevation");
+
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, "x")
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name("uBigWavesFrequencyX");
+
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, "y")
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name("uBigWavesFrequencyY");
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial);
--- 29-Raging_Sea/src/shaders/water/vertex.glsl
+++ 29-Raging_Sea/src/shaders/water/vertex.glsl
@@ -1,6 +1,16 @@
+uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // Elevation
+    float elevation = sin(modelPosition.x * uBigWavesFrequency.x) *
+            sin(modelPosition.z * uBigWavesFrequency.y) *
+            uBigWavesElevation;
+    modelPosition.y += elevation;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
