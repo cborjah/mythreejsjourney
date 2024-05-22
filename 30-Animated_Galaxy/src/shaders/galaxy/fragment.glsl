e9e2b61827cf8e0a21f953f015620e707b2a22aa
Christian Borja <cpborja32@gmail.com>
2024-05-21 17:32:10 -0700

    feat: add light point pattern to particles

--- 30-Animated_Galaxy/src/shaders/galaxy/fragment.glsl
+++ 30-Animated_Galaxy/src/shaders/galaxy/fragment.glsl
@@ -6,9 +6,14 @@ void main()
     // strength = 1.0 - strength;
 
     // Diffuse point
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength *= 2.0;
+    // strength = 1.0 - strength;
+
+    // Light point
     float strength = distance(gl_PointCoord, vec2(0.5));
-    strength *= 2.0;
     strength = 1.0 - strength;
+    strength = pow(strength, 10.0);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
     #include <colorspace_fragment>
