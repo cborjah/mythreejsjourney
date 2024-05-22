2486044db468c057425e0d92d78ee6b7775f8f6f
Christian Borja <cpborja32@gmail.com>
2024-05-21 17:15:34 -0700

    feat: draw particles as a disc

--- 30-Animated_Galaxy/src/shaders/galaxy/fragment.glsl
+++ 30-Animated_Galaxy/src/shaders/galaxy/fragment.glsl
@@ -1,5 +1,10 @@
 void main()
 {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    // Disc
+    float strength = distance(gl_PointCoord, vec2(0.5));
+    strength = step(0.5, strength);
+    strength = 1.0 - strength;
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
     #include <colorspace_fragment>
 }
