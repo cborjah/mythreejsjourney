b890dcceb4c8cdbf9094938f064835cd25809e57
Christian Borja <cpborja32@gmail.com>
2024-05-29 11:42:41 -0700

    feat: animate and repeat smoke texture

--- 32-Coffee_Smoke_Shader/src/script.js
+++ 32-Coffee_Smoke_Shader/src/script.js
@@ -95,12 +95,15 @@ smokeGeometry.scale(1.5, 6, 1.5);
 
 // Perlin Texture
 const perlinTexture = textureLoader.load("./perlin.png");
+perlinTexture.wrapS = THREE.RepeatWrapping;
+perlinTexture.wrapT = THREE.RepeatWrapping;
 
 // Material
 const smokeMaterial = new THREE.ShaderMaterial({
     vertexShader: coffeeSmokeVertexShader,
     fragmentShader: coffeeSmokeFragmentShader,
     uniforms: {
+        uTime: new THREE.Uniform(0),
         uPerlinTexture: new THREE.Uniform(perlinTexture)
     },
     side: THREE.DoubleSide,
@@ -120,6 +123,9 @@ const clock = new THREE.Clock();
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime();
+
+    // Update smoke
+    smokeMaterial.uniforms.uTime.value = elapsedTime;
 
     // Update controls
     controls.update();
--- 32-Coffee_Smoke_Shader/src/shaders/coffeeSmoke/fragment.glsl
+++ 32-Coffee_Smoke_Shader/src/shaders/coffeeSmoke/fragment.glsl
@@ -1,11 +1,18 @@
+uniform float uTime;
 uniform sampler2D uPerlinTexture;
 
 varying vec2 vUv;
 
 void main()
 {
+    // Scale and Animate
+    vec2 smokeUv = vUv; // You cannot directly modify a varying variable. Use a new variable to do that.
+    smokeUv.x *= 0.5;
+    smokeUv.y *= 0.3;
+    smokeUv.y -= uTime * 0.03;
+
     // Smoke
-    float smoke = texture(uPerlinTexture, vUv).r; // Since its a greyscale picture, r,g, and b are the same values. Therefore, only retrieve the 'r' channel.
+    float smoke = texture(uPerlinTexture, smokeUv).r; // Since its a greyscale picture, r,g, and b are the same values. Therefore, only retrieve the 'r' channel.
 
     gl_FragColor = vec4(1.0, 1.0, 1.0, smoke);
     #include <tonemapping_fragment>
