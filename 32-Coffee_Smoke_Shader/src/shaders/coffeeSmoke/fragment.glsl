549b778cf65bf8deebed0f0738958e8a07391f4b
Christian Borja <cpborja32@gmail.com>
2024-05-29 12:01:45 -0700

    feat: fade edges of smoke texture

--- 32-Coffee_Smoke_Shader/src/shaders/coffeeSmoke/fragment.glsl
+++ 32-Coffee_Smoke_Shader/src/shaders/coffeeSmoke/fragment.glsl
@@ -17,6 +17,13 @@ void main()
     // Remap
     smoke = smoothstep(0.4, 1.0, smoke); // smoothstep returns a value from 0 to 1 and it's clamped.
 
+    // Edges
+    // Use the smoothstep function to lower the alpha "smoothing" the edges.
+    smoke *= smoothstep(0.0, 0.1, vUv.x);
+    smoke *= smoothstep(1.0, 0.9, vUv.x);
+    smoke *= smoothstep(0.0, 0.1, vUv.y);
+    smoke *= smoothstep(1.0, 0.4, vUv.y);
+
     gl_FragColor = vec4(1.0, 1.0, 1.0, smoke);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
