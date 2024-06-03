d6767c13b38e91303a42c665942522e75abfa02b
Christian Borja <cpborja32@gmail.com>
2024-06-03 14:47:36 -0700

    feat: prevent fresnel value from changing with the object rotation

--- 33-Hologram_Shader/src/shaders/holographic/fragment.glsl
+++ 33-Hologram_Shader/src/shaders/holographic/fragment.glsl
@@ -13,7 +13,7 @@ void main()
     // NOTE: In this case the fresnel value should NOT change with the object rotation.
     // The viewDirection vector should be the same length as the normal vector, which is 1. Normalize value to accomplish this.
     vec3 viewDirection = normalize(vPosition - cameraPosition); // The cameraPosition variable is made available by Three.js
-    float fresnel = dot(viewDirection, vNormal); // The dot product returns 1 if the two vectors are parallel to each other, 0 if they're perpendicular, and -1 if they are in opposite directions.
+    float fresnel = dot(viewDirection, vNormal) + 1.0; // The dot product returns 1 if the two vectors are parallel to each other, 0 if they're perpendicular, and -1 if they are in opposite directions.
 
     // Final color
     gl_FragColor = vec4(1.0, 1.0, 1.0, fresnel);
--- 33-Hologram_Shader/src/shaders/holographic/vertex.glsl
+++ 33-Hologram_Shader/src/shaders/holographic/vertex.glsl
@@ -8,7 +8,13 @@ void main()
 
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
+    // Model normal
+    // NOTE: When the fourth value is 1.0, the vector is 'homogeneous' and all 3 transformations (translation, rotation, scale) will be applied.
+    // When the fourth value is 0.0, the vector is not 'homogeneous' and the translation won't be applied.
+    // This is ideal in the case of a normal, because the normal is not a position, it's a direction.
+    vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
     // Varyings
     vPosition = modelPosition.xyz;
-    vNormal = normal;
+    vNormal = modelNormal.xyz;
 }
