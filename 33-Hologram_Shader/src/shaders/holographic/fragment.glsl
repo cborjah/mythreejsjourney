64b78e2904db09299eeef852514d7c8af0af450e
Christian Borja <cpborja32@gmail.com>
2024-06-03 14:07:24 -0700

    feat: add stripes to material

--- 33-Hologram_Shader/src/shaders/holographic/fragment.glsl
+++ 33-Hologram_Shader/src/shaders/holographic/fragment.glsl
@@ -1,7 +1,13 @@
+varying vec3 vPosition;
+
 void main()
 {
+    // Stripes
+    float stripes = mod(vPosition.y * 20.0, 1.0);
+    stripes = pow(stripes, 3.0);
+
     // Final color
-    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+    gl_FragColor = vec4(stripes, stripes, stripes, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
