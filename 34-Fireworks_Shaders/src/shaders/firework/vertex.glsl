b3c15d33817d2234a0d77e10beb0cd4e29b574a9
Christian Borja <cpborja32@gmail.com>
2024-06-04 16:20:33 -0700

    feat: add exploding animation

--- 34-Fireworks_Shaders/src/shaders/firework/vertex.glsl
+++ 34-Fireworks_Shaders/src/shaders/firework/vertex.glsl
@@ -1,12 +1,28 @@
 uniform float uSize;
 uniform vec2 uResolution;
+uniform float uProgress;
 
 attribute float aSize;
 
+// value - The value you want to remap
+// originMin/originMax - The start and end of the original range
+// destinationMin/destinationMax - The start and end of the destination range
+float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax)
+{
+    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
+}
+
 void main()
 {
+    vec3 newPosition = position;
+
+    // Exploding
+    float explodingProgress = remap(uProgress, 0.0, 0.1, 0.0, 1.0);
+    explodingProgress = clamp(explodingProgress, 0.0, 1.0);
+    newPosition *= explodingProgress;
+
     // Final position
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     gl_Position = projectionMatrix * viewPosition;
 
