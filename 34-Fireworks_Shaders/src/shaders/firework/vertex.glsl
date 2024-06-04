f68b01b5de7dbbfd56e0d02fe5c842f312a05cfa
Christian Borja <cpborja32@gmail.com>
2024-06-04 16:24:40 -0700

    feat: smooth out end of explosion animation

--- 34-Fireworks_Shaders/src/shaders/firework/vertex.glsl
+++ 34-Fireworks_Shaders/src/shaders/firework/vertex.glsl
@@ -19,6 +19,7 @@ void main()
     // Exploding
     float explodingProgress = remap(uProgress, 0.0, 0.1, 0.0, 1.0);
     explodingProgress = clamp(explodingProgress, 0.0, 1.0);
+    explodingProgress = 1.0 - pow(1.0 - explodingProgress, 3.0);
     newPosition *= explodingProgress;
 
     // Final position
