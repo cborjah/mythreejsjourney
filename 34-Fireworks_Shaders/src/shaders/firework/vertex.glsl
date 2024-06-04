5ba2a1373482ce1e0123f63ddcf6540ece14542a
Christian Borja <cpborja32@gmail.com>
2024-06-04 14:45:36 -0700

    feat: apply perspective to particles, increase particle size

--- 34-Fireworks_Shaders/src/shaders/firework/vertex.glsl
+++ 34-Fireworks_Shaders/src/shaders/firework/vertex.glsl
@@ -6,5 +6,6 @@ void main()
     gl_Position = projectionMatrix * viewPosition;
 
     // Final size
-    gl_PointSize = 20.0;
+    gl_PointSize = 50.0;
+    gl_PointSize *= 1.0 / -viewPosition.z; // Add perspective to particles. They get bigger the closer the camera gets and vice versa.
 }
