3ef4f85b4a7da5ec92d762d62a080451473c8846
Christian Borja <cpborja32@gmail.com>
2024-06-05 16:56:37 -0700

    feat: apply modelMatrix transformation to normals

--- 35-Lights_Shading_Shaders/src/shaders/shading/vertex.glsl
+++ 35-Lights_Shading_Shaders/src/shaders/shading/vertex.glsl
@@ -6,7 +6,12 @@ void main()
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
+    // Model normal
+    // Mutiply the normals with the modelMatrix to fix the issue of the light following the object's rotation.
+    // Setting the fourth value of the vec4 to 0.0 applies only the rotation and scale, not the translation.
+    vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
     // Varyings
-    vNormal = normal;
+    vNormal = modelNormal.xyz;
 }
 
