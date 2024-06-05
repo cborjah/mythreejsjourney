5ac5902322520debce950f4ba14dc2a9597ce937
Christian Borja <cpborja32@gmail.com>
2024-06-05 15:52:17 -0700

    feat: add ambient light to scene

--- 35-Lights_Shading_Shaders/src/shaders/shading/fragment.glsl
+++ 35-Lights_Shading_Shaders/src/shaders/shading/fragment.glsl
@@ -1,11 +1,27 @@
 uniform vec3 uColor;
 
+#include ../includes/ambientLight.glsl
+
 void main()
 {
     vec3 color = uColor;
+
+    // Light
+    vec3 light = vec3(0.0);
+
+    // NOTE: You can add lights together to add them to the scene.
+    light += ambientLight(
+            vec3(1.0), // Light color
+            0.03 // Light intensity
+        );
+
+    // NOTE: A common mistake is to add the light to the color.
+    // Instead the object's color needs to be multiplied by the light.
+    color *= light;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
+
