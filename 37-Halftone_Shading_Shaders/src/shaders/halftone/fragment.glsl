338a15794f4f10b020284b0eb54191e36d2772c5
Christian Borja <cpborja32@gmail.com>
2024-06-07 13:48:33 -0700

    feat: add ambient and directional lights

--- 37-Halftone_Shading_Shaders/src/shaders/halftone/fragment.glsl
+++ 37-Halftone_Shading_Shaders/src/shaders/halftone/fragment.glsl
@@ -3,14 +3,37 @@ uniform vec3 uColor;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
+#include ../includes/ambientLight.glsl
+#include ../includes/directionalLight.glsl
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
     vec3 color = uColor;
 
+    // Lights
+    vec3 light = vec3(0.0);
+
+    light += ambientLight(
+            vec3(1.0), // Light color
+            1.0 // Light intensity
+        );
+
+    light += directionalLight(
+            vec3(1.0, 1.0, 1.0),
+            1.0,
+            normal,
+            vec3(1.0, 1.0, 0.0),
+            viewDirection,
+            1.0
+        );
+
+    color *= light;
+
     // Final color
     gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
+
