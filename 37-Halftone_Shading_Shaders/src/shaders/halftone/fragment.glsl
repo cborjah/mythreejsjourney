832b4c52e3af035b1d70993755ae5fd96e7e76ae
Christian Borja <cpborja32@gmail.com>
2024-06-07 15:19:03 -0700

    chore: code clean up

--- 37-Halftone_Shading_Shaders/src/shaders/halftone/fragment.glsl
+++ 37-Halftone_Shading_Shaders/src/shaders/halftone/fragment.glsl
@@ -43,8 +43,6 @@ void main()
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
-    // gl_FragColor = vec4(uv, 1.0, 1.0);
-    // gl_FragColor = vec4(point, point, point, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
