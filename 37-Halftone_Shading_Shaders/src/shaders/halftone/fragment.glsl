150e67096c306dc3584aae89c2ae48c2750f10a5
Christian Borja <cpborja32@gmail.com>
2024-06-07 15:15:59 -0700

    refactor: move halftone logic into its own file

--- 37-Halftone_Shading_Shaders/src/shaders/halftone/fragment.glsl
+++ 37-Halftone_Shading_Shaders/src/shaders/halftone/fragment.glsl
@@ -10,24 +10,7 @@ varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 #include ../includes/directionalLight.glsl
-
-vec3 halftone(vec3 color, float repetitions, vec3 direction, float low, float high, vec3 pointColor, vec3 normal)
-{
-    float intensity = dot(normal, direction);
-    intensity = smoothstep(low, high, intensity);
-
-    // NOTE: gl_FragCoord contains the window-relative coordinates of the current fragment.
-    // When you divide a vec2 by ONE float, it will divide both the x and the y.
-    // This is done to maintain the square shape of each grid.
-    vec2 uv = gl_FragCoord.xy / uResolution.y; // Dividing by y will only allow resizing of the grid when the window height changes.
-    uv *= repetitions; // Controls the amount of cells vertically.
-    uv = mod(uv, 1.0);
-
-    float point = distance(uv, vec2(0.5));
-    point = 1.0 - step(0.5 * intensity, point);
-
-    return mix(color, pointColor, point); // Mix color with pointColor depending on the point value. If point is 0 you get color. If point is 1 you get pointColor.
-}
+#include ../includes/halftone.glsl
 
 void main()
 {
