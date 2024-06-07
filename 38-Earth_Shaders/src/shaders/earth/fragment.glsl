5cce5b61072306adfa8e265a9edc512881b11492
Christian Borja <cpborja32@gmail.com>
2024-06-07 16:04:34 -0700

    feat: add day/night colors using the textures

--- 38-Earth_Shaders/src/shaders/earth/fragment.glsl
+++ 38-Earth_Shaders/src/shaders/earth/fragment.glsl
@@ -10,7 +10,17 @@ void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
+    vec3 color = vec3(0.0);
+
+    // Sun orientation
+    vec3 uSunDirection = vec3(0.0, 0.0, 1.0);
+    float sunOrientation = dot(uSunDirection, normal);
+
+    // Day / Night color
+    float dayMix = smoothstep(-0.25, 0.5, sunOrientation);
+    vec3 dayColor = texture(uDayTexture, vUv).rgb;
+    vec3 nightColor = texture(uNightTexture, vUv).rgb;
+    color = mix(nightColor, dayColor, dayMix);
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
