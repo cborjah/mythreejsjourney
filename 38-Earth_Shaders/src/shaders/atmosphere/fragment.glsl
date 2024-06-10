bf27f0fc8c83988b0cb6643de4e681d304cfebd2
Christian Borja <cpborja32@gmail.com>
2024-06-10 14:17:58 -0700

    feat: create atmosphere shader using earth shader

---
+++ 38-Earth_Shaders/src/shaders/atmosphere/fragment.glsl
@@ -0,0 +1,26 @@
+uniform vec3 uSunDirection;
+uniform vec3 uAtmosphereDayColor;
+uniform vec3 uAtmosphereTwilightColor;
+
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+void main()
+{
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+    vec3 normal = normalize(vNormal);
+    vec3 color = vec3(0.0);
+
+    // Sun orientation
+    float sunOrientation = dot(uSunDirection, normal);
+
+    // Atmosphere
+    float atmosphereDayMix = smoothstep(-0.5, 1.0, sunOrientation);
+    vec3 atmosphereColor = mix(uAtmosphereTwilightColor, uAtmosphereDayColor, atmosphereDayMix);
+    color = mix(color, atmosphereColor, atmosphereDayMix);
+
+    // Final color
+    gl_FragColor = vec4(color, 1.0);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
