bbfee3d41364dc45045cb19ebf2f59fb3fbc9202
Christian Borja <cpborja32@gmail.com>
2024-06-12 14:43:44 -0700

    feat: pass texture as uniform and render discs size based on texture color

--- 39-Particles_Cursor_Animation_Shader/src/script.js
+++ 39-Particles_Cursor_Animation_Shader/src/script.js
@@ -1,19 +1,19 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import particlesVertexShader from './shaders/particles/vertex.glsl'
-import particlesFragmentShader from './shaders/particles/fragment.glsl'
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import particlesVertexShader from "./shaders/particles/vertex.glsl";
+import particlesFragmentShader from "./shaders/particles/fragment.glsl";
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Loaders
-const textureLoader = new THREE.TextureLoader()
+const textureLoader = new THREE.TextureLoader();
 
 /**
  * Sizes
@@ -22,38 +22,45 @@ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
     pixelRatio: Math.min(window.devicePixelRatio, 2)
-}
+};
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener("resize", () => {
     // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
     // Materials
-    particlesMaterial.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
+    particlesMaterial.uniforms.uResolution.value.set(
+        sizes.width * sizes.pixelRatio,
+        sizes.height * sizes.pixelRatio
+    );
 
     // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
     // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(sizes.pixelRatio)
-})
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(sizes.pixelRatio);
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0, 0, 18)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+    35,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+camera.position.set(0, 0, 18);
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
@@ -61,40 +68,47 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true
-})
-renderer.setClearColor('#181818')
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(sizes.pixelRatio)
+});
+renderer.setClearColor("#181818");
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(sizes.pixelRatio);
 
 /**
  * Particles
  */
-const particlesGeometry = new THREE.PlaneGeometry(10, 10, 32, 32)
+const particlesGeometry = new THREE.PlaneGeometry(10, 10, 32, 32);
 
 const particlesMaterial = new THREE.ShaderMaterial({
     vertexShader: particlesVertexShader,
     fragmentShader: particlesFragmentShader,
-    uniforms:
-    {
-        uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+    uniforms: {
+        uResolution: new THREE.Uniform(
+            new THREE.Vector2(
+                sizes.width * sizes.pixelRatio,
+                sizes.height * sizes.pixelRatio
+            )
+        ),
+        uPictureTexture: new THREE.Uniform(
+            textureLoader.load("./picture-1.png")
+        )
     }
-})
-const particles = new THREE.Points(particlesGeometry, particlesMaterial)
-scene.add(particles)
+});
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Animate
  */
-const tick = () =>
-{
+const tick = () => {
     // Update controls
-    controls.update()
+    controls.update();
 
     // Render
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+    window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
--- 39-Particles_Cursor_Animation_Shader/src/shaders/particles/vertex.glsl
+++ 39-Particles_Cursor_Animation_Shader/src/shaders/particles/vertex.glsl
@@ -1,14 +1,22 @@
 uniform vec2 uResolution;
+uniform sampler2D uPictureTexture;
 
 void main()
 {
+    // Use the texture() function to pick the color from the uPictureTexture at the uv coordinates
+    // (whole geometry, not individual particles) and swizzle the r channel.
+
     // Final position
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
 
+    // Picture
+    float pictureIntensity = texture(uPictureTexture, uv).r;
+
     // Point size
-    gl_PointSize = 0.3 * uResolution.y;
-    gl_PointSize *= (1.0 / - viewPosition.z);
+    gl_PointSize = 0.3 * pictureIntensity * uResolution.y;
+    gl_PointSize *= (1.0 / -viewPosition.z);
 }
+
