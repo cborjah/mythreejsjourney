886c07762cc3ae04f38cf4c03b43a55c079e87ec
Christian Borja <cpborja32@gmail.com>
2024-06-14 15:36:51 -0700

    feat: use particle's uv coordinates to render a 'point light' texture

--- 40-Particles_Morphing_Shader/src/shaders/particles/fragment.glsl
+++ 40-Particles_Morphing_Shader/src/shaders/particles/fragment.glsl
@@ -1,6 +1,11 @@
 void main()
 {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    vec2 uv = gl_PointCoord;
+    float distanceToCenter = length(uv - 0.5);
+    float alpha = 0.05 / distanceToCenter - 0.1; // Divide by small number technique.
+
+    gl_FragColor = vec4(alpha, alpha, alpha, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
+
