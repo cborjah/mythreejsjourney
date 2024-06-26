4ba1cd97e28e00e2f8c0e38e5b623ce6bd77b5cf
Christian Borja <cpborja32@gmail.com>
2024-06-26 14:08:53 -0700

    feat: disable frustum culling for particles to prevent particles from disappearing due to bounding not being updated

--- 40-Particles_Morphing_Shader/src/script.js
+++ 40-Particles_Morphing_Shader/src/script.js
@@ -199,6 +199,7 @@ gltfLoader.load("./models.glb", (gltf) => {
 
     // Points
     particles.points = new THREE.Points(particles.geometry, particles.material);
+    particles.points.frustumCulled = false;
     scene.add(particles.points);
 
     // Methods
