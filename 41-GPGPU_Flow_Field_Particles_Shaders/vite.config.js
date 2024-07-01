3e1551bb35b971524e889d5ae629afc950a62190
Christian Borja <cpborja32@gmail.com>
2024-07-01 15:26:31 -0700

    config: set Vite to build with compatibility with modern browsers

--- 41-GPGPU_Flow_Field_Particles_Shaders/vite.config.js
+++ 41-GPGPU_Flow_Field_Particles_Shaders/vite.config.js
@@ -1,24 +1,24 @@
-import restart from 'vite-plugin-restart'
-import glsl from 'vite-plugin-glsl'
+import restart from "vite-plugin-restart";
+import glsl from "vite-plugin-glsl";
 
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
+    root: "src/",
+    publicDir: "../static/",
+    base: "./",
+    server: {
         host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+        open: !(
+            "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env
+        ) // Open if it's not a CodeSandbox
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
+    build: {
+        outDir: "../dist", // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
+        target: "esnext"
     },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }), // Restart server on static file change
+    plugins: [
+        restart({ restart: ["../static/**"] }), // Restart server on static file change
         glsl() // Handle shader files
     ]
-}
+};
