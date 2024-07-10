aeb48c8a424b699c2cac466375798be6046ac005
Christian Borja <cpborja32@gmail.com>
2024-07-10 16:21:12 -0700

    fix: fix angle issue

--- 43-Sliced_Model_Shader/src/shaders/sliced/fragment.glsl
+++ 43-Sliced_Model_Shader/src/shaders/sliced/fragment.glsl
@@ -7,8 +7,13 @@ void main()
 {
     // NOTE: The y coordinate must come before the x coordinate.
     float angle = atan(vPosition.y, vPosition.x);
+    angle -= uSliceStart;
+    angle = mod(angle, PI2); // Updated the arc tangent angle to get a range from 0 to 2*PI (PI2). Only positive values for the angle.
 
-    if (angle > uSliceStart && angle < uSliceStart + uSliceArc) {
+    // NOTE: Using modulo on negative numbers can result in a different behavior based on the environment.
+    // -0.25 % 1 in JS will output -0.25 and not 0.75 like in GLSL.
+
+    if (angle > 0.0 && angle < uSliceArc) {
         discard;
     }
 
