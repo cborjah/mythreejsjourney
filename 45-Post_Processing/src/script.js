4fafc9469f2a254b72359738ea801721acd5ca05
Christian Borja <cpborja32@gmail.com>
2024-07-16 13:34:01 -0700

    feat: add DotScreenPass effect

--- 45-Post_Processing/src/script.js
+++ 45-Post_Processing/src/script.js
@@ -3,6 +3,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { DotScreenPass } from "three/examples/jsm/postprocessing/DotScreenPass.js";
 import GUI from "lil-gui";
 
 /**
@@ -158,6 +159,10 @@ effectComposer.setSize(sizes.width, sizes.height);
 
 const renderPass = new RenderPass(scene, camera);
 effectComposer.addPass(renderPass);
+
+const dotScreenPass = new DotScreenPass();
+dotScreenPass.enabled = false;
+effectComposer.addPass(dotScreenPass);
 
 /**
  * Animate
