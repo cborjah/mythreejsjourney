206a70442d1ae8da4d1160c3cc5d2abad7ad8b9d
Christian Borja <cpborja32@gmail.com>
2024-07-16 14:45:41 -0700

    feat: add Unreal Bloom pass

--- 45-Post_Processing/src/script.js
+++ 45-Post_Processing/src/script.js
@@ -9,6 +9,7 @@ import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
 import { RGBShiftShader } from "three/examples/jsm/shaders/RGBShiftShader.js";
 import { GammaCorrectionShader } from "three/examples/jsm/shaders/GammaCorrectionShader.js";
 import { SMAAPass } from "three/examples/jsm/postprocessing/SMAAPass.js";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
 import GUI from "lil-gui";
 
 /**
@@ -203,13 +204,26 @@ effectComposer.addPass(dotScreenPass);
 // Glitch pass
 const glitchPass = new GlitchPass();
 // glitchPass.goWild = true;
-// glitchPass.enabled = false;
+glitchPass.enabled = false;
 effectComposer.addPass(glitchPass);
 
 // RGB Shift pass
 const rgbShiftPass = new ShaderPass(RGBShiftShader);
 rgbShiftPass.enabled = false;
 effectComposer.addPass(rgbShiftPass);
+
+// Unreal Bloom Pass
+// NOTE: Must be BEFORE AA and Gamma Correction passes!
+const unrealBloomPass = new UnrealBloomPass();
+unrealBloomPass.strength = 0.3;
+unrealBloomPass.radius = 1;
+unrealBloomPass.threshold = 0.6;
+effectComposer.addPass(unrealBloomPass);
+
+gui.add(unrealBloomPass, "enabled");
+gui.add(unrealBloomPass, "strength", 0, 2).step(0.001);
+gui.add(unrealBloomPass, "radius", 0, 2).step(0.001);
+gui.add(unrealBloomPass, "threshold", 0, 2).step(0.001);
 
 // Gamma Correction pass
 const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
