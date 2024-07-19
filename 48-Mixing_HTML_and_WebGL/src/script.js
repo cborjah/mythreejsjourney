c9a9bea0e4dca727f1b49e9c8d9300a1e5381c1c
Christian Borja <cpborja32@gmail.com>
2024-07-19 13:50:59 -0700

    feat: prevent points from rendering before scene is fully loaded

--- 48-Mixing_HTML_and_WebGL/src/script.js
+++ 48-Mixing_HTML_and_WebGL/src/script.js
@@ -6,6 +6,7 @@ import { gsap } from "gsap";
 /**
  * Loaders
  */
+let sceneReady = false;
 const loadingBarElement = document.querySelector(".loading-bar");
 const loadingManager = new THREE.LoadingManager(
     // Loaded
@@ -23,6 +24,10 @@ const loadingManager = new THREE.LoadingManager(
             loadingBarElement.classList.add("ended");
             loadingBarElement.style.transform = "";
         }, 500);
+
+        window.setTimeout(() => {
+            sceneReady = true;
+        }, 3000);
     },
 
     // Progress
@@ -205,33 +210,37 @@ const tick = () => {
     // Update controls
     controls.update();
 
-    // Go through each point
-    for (const point of points) {
-        // Retrieve the 2D screen position of the 3D scene position of the point
-        // Clone the point's position. When converting it to screen coordinates you don't want to mutate the original position.
-        const screenPosition = point.position.clone();
-        screenPosition.project(camera);
+    if (sceneReady) {
+        // Go through each point
+        for (const point of points) {
+            // Retrieve the 2D screen position of the 3D scene position of the point
+            // Clone the point's position. When converting it to screen coordinates you don't want to mutate the original position.
+            const screenPosition = point.position.clone();
+            screenPosition.project(camera);
 
-        raycaster.setFromCamera(screenPosition, camera); // Automatically uses only the x and y values
-        const intersects = raycaster.intersectObjects(scene.children, true); // The second parameter is for enabling recursion. This will check nested children.
+            raycaster.setFromCamera(screenPosition, camera); // Automatically uses only the x and y values
+            const intersects = raycaster.intersectObjects(scene.children, true); // The second parameter is for enabling recursion. This will check nested children.
 
-        if (intersects.length === 0) {
-            point.element.classList.add("visible");
-        } else {
-            const intersectionDistance = intersects[0].distance;
-            const pointDistance = point.position.distanceTo(camera.position);
-
-            if (intersectionDistance < pointDistance) {
-                // Something is in front of the point
-                point.element.classList.remove("visible");
-            } else {
+            if (intersects.length === 0) {
                 point.element.classList.add("visible");
-            }
-        }
+            } else {
+                const intersectionDistance = intersects[0].distance;
+                const pointDistance = point.position.distanceTo(
+                    camera.position
+                );
 
-        const translateX = screenPosition.x * sizes.width * 0.5;
-        const translateY = -screenPosition.y * sizes.height * 0.5;
-        point.element.style.transform = `translate(${translateX}px, ${translateY}px)`;
+                if (intersectionDistance < pointDistance) {
+                    // Something is in front of the point
+                    point.element.classList.remove("visible");
+                } else {
+                    point.element.classList.add("visible");
+                }
+            }
+
+            const translateX = screenPosition.x * sizes.width * 0.5;
+            const translateY = -screenPosition.y * sizes.height * 0.5;
+            point.element.style.transform = `translate(${translateX}px, ${translateY}px)`;
+        }
     }
 
     // Render
