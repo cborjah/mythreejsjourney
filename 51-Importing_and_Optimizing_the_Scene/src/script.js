6f4dfa73ace9a998f885939538e83daaa1cf04b5
Christian Borja <cpborja32@gmail.com>
2024-07-24 15:53:37 -0700

    feat: add background color to scene

--- 51-Importing_and_Optimizing_the_Scene/src/script.js
+++ 51-Importing_and_Optimizing_the_Scene/src/script.js
@@ -8,6 +8,7 @@ import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
  * Base
  */
 // Debug
+const debugObject = {};
 const gui = new GUI({
     width: 400
 });
@@ -126,6 +127,12 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+debugObject.clearColor = "#201919";
+renderer.setClearColor(debugObject.clearColor);
+gui.addColor(debugObject, "clearColor").onChange(() =>
+    renderer.setClearColor(debugObject.clearColor)
+);
 
 /**
  * Animate
