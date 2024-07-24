908cd39d7752f78d3e4a161c9c66e06b9f27f538
Christian Borja <cpborja32@gmail.com>
2024-07-24 14:55:10 -0700

    feat: apply texture onto model and fix coloring

--- 51-Importing_and_Optimizing_the_Scene/src/script.js
+++ 51-Importing_and_Optimizing_the_Scene/src/script.js
@@ -33,10 +33,17 @@ const gltfLoader = new GLTFLoader();
 gltfLoader.setDRACOLoader(dracoLoader);
 
 /**
+ * Textures
+ */
+const bakedTexture = textureLoader.load("my_baked.jpg");
+bakedTexture.flipY = false;
+bakedTexture.colorSpace = THREE.SRGBColorSpace;
+
+/**
  * Materials
  */
 // Baked Material
-const bakedMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture });
 
 /**
  * Model
