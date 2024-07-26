ce8e7c926a16213b74b81c48e4970527d3537e83
Christian Borja <cpborja32@gmail.com>
2024-07-26 15:16:54 -0700

    feat: add rotation animation to cube

--- 55-First_R3F_Application/src/Experience.js
+++ 55-First_R3F_Application/src/Experience.js
@@ -1,4 +1,15 @@
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
 export default function Experience() {
+    const cubeRef = useRef();
+
+    // useFrame will be called on each frame BEFORE rendering the scene regardless of current frame rat regardless of current frame rate.
+    useFrame((state, delta) => {
+        // delta is how much time has passed since the last frame
+        cubeRef.current.rotation.y += delta;
+    });
+
     /**
      * Geometry construction parameters can be changed by providing an array to the 'args'
      * attribute, in this case following the order (radius, widthSegments, heightSegments).
@@ -11,16 +22,24 @@ export default function Experience() {
      *
      * The order of meshes aren't relevant unlike in native Three.js.
      */
+
     return (
         <>
             <mesh position-x={-2}>
                 <sphereGeometry />
                 <meshBasicMaterial color="orange" />
             </mesh>
-            <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+
+            <mesh
+                ref={cubeRef}
+                rotation-y={Math.PI * 0.25}
+                position-x={2}
+                scale={1.5}
+            >
                 <boxGeometry scale={1.5} />
                 <meshBasicMaterial color="mediumpurple" />
             </mesh>
+
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
                 <meshBasicMaterial color="greenyellow" />
