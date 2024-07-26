bb32c527e302053bdd055290baa4c27d28adf970
Christian Borja <cpborja32@gmail.com>
2024-07-26 15:08:35 -0700

    feat: add more meshes to scene

--- 55-First_R3F_Application/src/Experience.js
+++ 55-First_R3F_Application/src/Experience.js
@@ -1,9 +1,29 @@
 export default function Experience() {
+    /**
+     * Geometry construction parameters can be changed by providing an array to the 'args'
+     * attribute, in this case following the order (radius, widthSegments, heightSegments).
+     *
+     * Use the Three.js documentation as a reference on how to pass in the args.
+     *
+     * NOTE: In the case of geometries, take care not to update their values
+     * too often or animate them. Each change will result in the whole
+     * geometry being destroyed and recreated.
+     *
+     * The order of meshes aren't relevant unlike in native Three.js.
+     */
     return (
         <>
-            <mesh>
-                <torusKnotGeometry />
-                <meshNormalMaterial />
+            <mesh position-x={-2}>
+                <sphereGeometry />
+                <meshBasicMaterial color="orange" />
+            </mesh>
+            <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+                <boxGeometry scale={1.5} />
+                <meshBasicMaterial color="mediumpurple" />
+            </mesh>
+            <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+                <planeGeometry />
+                <meshBasicMaterial color="greenyellow" />
             </mesh>
         </>
     );
