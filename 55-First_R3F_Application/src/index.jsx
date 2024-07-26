24158a9ee854841b7c29790012db5d10ea13812c
Christian Borja <cpborja32@gmail.com>
2024-07-26 16:00:49 -0700

    feat: customize camera settings

--- 55-First_R3F_Application/src/index.jsx
+++ 55-First_R3F_Application/src/index.jsx
@@ -7,7 +7,14 @@ import Experience from "./Experience";
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-    <Canvas>
+    <Canvas
+        camera={{
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [3, 2, 6]
+        }}
+    >
         <Experience />
     </Canvas>
 );
