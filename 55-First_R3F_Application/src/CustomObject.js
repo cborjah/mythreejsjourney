f18826be37f37bf95a03d96d393c90d0e42f02eb
Christian Borja <cpborja32@gmail.com>
2024-07-26 15:48:21 -0700

    perf: memoize generation of custom geometry positions

--- 55-First_R3F_Application/src/CustomObject.js
+++ 55-First_R3F_Application/src/CustomObject.js
@@ -1,12 +1,18 @@
+import { useMemo } from "react";
 import * as THREE from "three";
 
 export default function CustomObject() {
     const verticesCount = 10 * 3; // 3 vertices per triangle
-    const positions = new Float32Array(verticesCount * 3); // Each vertex needs 3 values
 
-    for (let i = 0; i < verticesCount * 3; i++) {
-        positions[i] = (Math.random() - 0.5) * 3;
-    }
+    const positions = useMemo(() => {
+        const positions = new Float32Array(verticesCount * 3); // Each vertex needs 3 values
+
+        for (let i = 0; i < verticesCount * 3; i++) {
+            positions[i] = (Math.random() - 0.5) * 3;
+        }
+
+        return positions;
+    }, []);
 
     return (
         <mesh>
