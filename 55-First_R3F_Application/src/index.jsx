60f4c90e3b1925bd8d29d7fa16425a02edd48763
Christian Borja <cpborja32@gmail.com>
2024-07-26 16:10:30 -0700

    feat: add antialias setting to canvas

--- 55-First_R3F_Application/src/index.jsx
+++ 55-First_R3F_Application/src/index.jsx
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
     <Canvas
+        gl={{ antialias: true }}
         camera={{
             fov: 45,
             near: 0.1,
