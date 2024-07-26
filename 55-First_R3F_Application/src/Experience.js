4390d6e1fc7fe26cc7ed9f508907c240f50f7219
Christian Borja <cpborja32@gmail.com>
2024-07-26 15:33:28 -0700

    feat: add lights to scene

--- 55-First_R3F_Application/src/Experience.js
+++ 55-First_R3F_Application/src/Experience.js
@@ -40,10 +40,13 @@ export default function Experience() {
         <>
             <orbitControls args={[camera, gl.domElement]} />
 
+            <directionalLight position={[1, 2, 3]} intensity={4.5} />
+            <ambientLight intensity={1.5} />
+
             <group ref={groupRef}>
                 <mesh position-x={-2}>
                     <sphereGeometry />
-                    <meshBasicMaterial color="orange" />
+                    <meshStandardMaterial color="orange" />
                 </mesh>
 
                 <mesh
@@ -53,13 +56,13 @@ export default function Experience() {
                     scale={1.5}
                 >
                     <boxGeometry scale={1.5} />
-                    <meshBasicMaterial color="mediumpurple" />
+                    <meshStandardMaterial color="mediumpurple" />
                 </mesh>
             </group>
 
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
-                <meshBasicMaterial color="greenyellow" />
+                <meshStandardMaterial color="greenyellow" />
             </mesh>
         </>
     );
