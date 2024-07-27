2f272cbc6570ab7911035e89ed7c2bb1599cb82f
Christian Borja <cpborja32@gmail.com>
2024-07-27 11:50:00 -0700

    refactor: change to OrbitControls from drei library

--- 56-R3F_Drei/src/Experience.jsx
+++ 56-R3F_Drei/src/Experience.jsx
@@ -1,6 +1,10 @@
+import { OrbitControls } from "@react-three/drei";
+
 export default function Experience() {
     return (
         <>
+            <OrbitControls />
+
             <directionalLight position={[1, 2, 3]} intensity={4.5} />
             <ambientLight intensity={1.5} />
 
