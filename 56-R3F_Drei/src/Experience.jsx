f98a7423748b63f7228030d459f3b3356b1cf818
Christian Borja <cpborja32@gmail.com>
2024-07-27 12:09:06 -0700

    feat: add TransformControls to scene

--- 56-R3F_Drei/src/Experience.jsx
+++ 56-R3F_Drei/src/Experience.jsx
@@ -1,9 +1,21 @@
-import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import { OrbitControls, TransformControls } from "@react-three/drei";
+
+/**
+ * TransformControls should be placed after the object it will be applied to.
+ *
+ * NOTE: Setting the 'makeDefault' property for OrbitControls to true in order to
+ * prevent the camera from moving when using TransformControls. It tells
+ * Three.js that it is the default control. This allows other helpers to
+ * access the default controls and decide whether or not to deactivate it.
+ */
 
 export default function Experience() {
+    const cubeRef = useRef();
+
     return (
         <>
-            <OrbitControls />
+            <OrbitControls makeDefault />
 
             <directionalLight position={[1, 2, 3]} intensity={4.5} />
             <ambientLight intensity={1.5} />
@@ -13,10 +25,12 @@ export default function Experience() {
                 <meshStandardMaterial color="orange" />
             </mesh>
 
-            <mesh position-x={2} scale={1.5}>
+            <mesh ref={cubeRef} position-x={2} scale={1.5}>
                 <boxGeometry />
                 <meshStandardMaterial color="mediumpurple" />
             </mesh>
+
+            <TransformControls object={cubeRef} mode="translate" />
 
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
