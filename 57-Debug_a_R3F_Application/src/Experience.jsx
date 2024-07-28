b5645241d1aacef485872976a6d4f9f64bda1274
Christian Borja <cpborja32@gmail.com>
2024-07-28 13:59:39 -0700

    style: format Experience.jsx

--- 57-Debug_a_R3F_Application/src/Experience.jsx
+++ 57-Debug_a_R3F_Application/src/Experience.jsx
@@ -1,28 +1,27 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls } from "@react-three/drei";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+    return (
+        <>
+            <OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
+            <directionalLight position={[1, 2, 3]} intensity={4.5} />
+            <ambientLight intensity={1.5} />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+            <mesh position-x={-2}>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+            <mesh position-x={2} scale={1.5}>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+            <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+                <planeGeometry />
+                <meshStandardMaterial color="greenyellow" />
+            </mesh>
+        </>
+    );
 }
