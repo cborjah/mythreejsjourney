0797a57e3be6c42da71d7a487f6e530ad549bb89
Christian Borja <cpborja32@gmail.com>
2024-07-29 15:34:06 -0700

    feat: implement draco loader and use draco compressed model

--- 59-Load_Models_with_R3F/src/Experience.jsx
+++ 59-Load_Models_with_R3F/src/Experience.jsx
@@ -2,9 +2,14 @@ import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 
 export default function Experience() {
-    const model = useLoader(GLTFLoader, "./hamburger.glb");
+    const model = useLoader(GLTFLoader, "./hamburger-draco.glb", (loader) => {
+        const dracoLoader = new DRACOLoader();
+        dracoLoader.setDecoderPath("./draco/");
+        loader.setDRACOLoader(dracoLoader);
+    });
 
     return (
         <>
