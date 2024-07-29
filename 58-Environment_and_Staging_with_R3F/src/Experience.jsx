fc504a35ce23417a7347fd267788b4aae9f74804
Christian Borja <cpborja32@gmail.com>
2024-07-29 12:20:53 -0700

    feat: configure shadows

--- 58-Environment_and_Staging_with_R3F/src/Experience.jsx
+++ 58-Environment_and_Staging_with_R3F/src/Experience.jsx
@@ -30,6 +30,13 @@ export default function Experience() {
                 position={[1, 2, 3]}
                 intensity={4.5}
                 castShadow={true}
+                shadow-mapSize={[1024, 1024]}
+                shadow-camera-near={1}
+                shadow-camera-far={10}
+                shadow-camera-top={5}
+                shadow-camera-right={5}
+                shadow-camera-bottom={-5}
+                shadow-camera-left={-5}
             />
             <ambientLight intensity={1.5} />
 
