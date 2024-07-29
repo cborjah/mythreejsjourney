b6eb75a21683ce839622b0a7cf0c310b1829bc40
Christian Borja <cpborja32@gmail.com>
2024-07-29 14:51:37 -0700

    feat: implement Stage in scene

--- 58-Environment_and_Staging_with_R3F/src/Experience.jsx
+++ 58-Environment_and_Staging_with_R3F/src/Experience.jsx
@@ -9,7 +9,8 @@ import {
     ContactShadows,
     // Sky,
     Environment,
-    Lightformer
+    Lightformer,
+    Stage
 } from "@react-three/drei";
 import { useRef, useEffect } from "react";
 import { Perf } from "r3f-perf";
@@ -17,12 +18,14 @@ import * as THREE from "three";
 import { useControls } from "leva";
 
 /**
+ *
  * Percent Closer Soft Shadows (PCSS)
  *
  * Makes shadows look blurry by picking the shadow map texture at an offset
  * position according to the distance between the surface casting the shadow
  * and the surface receiving the shadow.
  *
+ **********************************************************************************
  *
  * Accumulative Shadows
  *
@@ -34,6 +37,7 @@ import { useControls } from "leva";
  *       Make sure to place it slightly above the plane to prevent z-fighting.
  *       Requires its own lights!
  *
+ **********************************************************************************
  *
  *  Contact Shadows
  *
@@ -47,10 +51,19 @@ import { useControls } from "leva";
  *        Blurs the shadow regardless of distance from objects.
  *        Impacts performance.
  *
+ **********************************************************************************
  *
  *  Sun
  *
- *  Use spherical coordinates when setting the sun position.
+ *  NOTE: Use spherical coordinates when setting the sun position.
+ *
+ **********************************************************************************
+ *
+ *  Stage
+ *
+ *  Stage will set an environment map, shadows, two directional lights
+ *  and center the scene.
+ *
  */
 
 export default function Experience() {
@@ -84,15 +97,15 @@ export default function Experience() {
 
     // Update the envMapIntensity of ALL the materials without having to set
     // it manually on each one by updating the scene's envMapIntensity.
-    useEffect(() => {
-        scene.environmentIntensity = envMapIntensity;
-    }, [envMapIntensity]);
+    // useEffect(() => {
+    //     scene.environmentIntensity = envMapIntensity;
+    // }, [envMapIntensity]);
 
     const scene = useThree((state) => state.scene);
 
     return (
         <>
-            <Environment
+            {/* <Environment
                 // background={true}
                 // files={"./environmentMaps/the_sky_is_on_fire_2k.hdr"}
                 preset={"sunset"} // Comes with a list of hdr's for immediate use.
@@ -111,17 +124,17 @@ export default function Experience() {
                     intensity={10}
                     form="ring"
                 />
-                {/* <mesh position-z={-5} scale={10}>
-                    <planeGeometry />
-                    <meshBasicMaterial color={[10, 0, 0]} />
-                </mesh> */}
-            </Environment>
+                // <mesh position-z={-5} scale={10}>
+                //    <planeGeometry />
+                //    <meshBasicMaterial color={[10, 0, 0]} />
+                // </mesh>
+            </Environment> */}
 
             {/*<BakeShadows />*/}
             {/*<SoftShadows size={25} samples={17} focus={0.25} />*/}
 
             {/* Another way to add a background color. It must be attached for it to work. */}
-            <color args={["ivory"]} attach="background" />
+            {/* <color args={["ivory"]} attach="background" /> */}
 
             <Perf position="top-left" />
 
@@ -148,7 +161,7 @@ export default function Experience() {
                 </AccumulativeShadows>
            */}
 
-            <ContactShadows
+            {/* <ContactShadows
                 position={[0, 0, 0]}
                 scale={10}
                 resolution={512}
@@ -157,7 +170,7 @@ export default function Experience() {
                 opacity={opacity}
                 blur={blur}
                 frames={1}
-            />
+            /> */}
 
             {/*<directionalLight
                 ref={directionalLight}
@@ -176,21 +189,32 @@ export default function Experience() {
 
             {/* <Sky sunPosition={sunPosition} /> */}
 
-            <mesh position-x={-2} position-y={1} castShadow={true}>
-                <sphereGeometry />
-                <meshStandardMaterial color="orange" />
-            </mesh>
-
-            <mesh
-                ref={cube}
-                position-x={2}
-                position-y={1}
-                scale={1.5}
-                castShadow={true}
+            <Stage
+                shadows={{
+                    type: "contact",
+                    opacity: 0.2,
+                    blur: 3
+                }}
+                environment="sunset"
+                preset="portrait"
+                intensity={envMapIntensity}
             >
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
+                <mesh position-x={-2} position-y={1} castShadow={true}>
+                    <sphereGeometry />
+                    <meshStandardMaterial color="orange" />
+                </mesh>
+
+                <mesh
+                    ref={cube}
+                    position-x={2}
+                    position-y={1}
+                    scale={1.5}
+                    castShadow={true}
+                >
+                    <boxGeometry />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+            </Stage>
 
             {/* <mesh
                 position-y={0}
