483ca2cace105e13b7396a9d8fd82c73a34dab4f
Christian Borja <cpborja32@gmail.com>
2024-07-29 13:32:10 -0700

    feat: implement AccumulativeShadows

--- 58-Environment_and_Staging_with_R3F/src/Experience.jsx
+++ 58-Environment_and_Staging_with_R3F/src/Experience.jsx
@@ -3,7 +3,9 @@ import {
     OrbitControls,
     useHelper,
     BakeShadows,
-    SoftShadows
+    SoftShadows,
+    AccumulativeShadows,
+    RandomizedLight
 } from "@react-three/drei";
 import { useRef } from "react";
 import { Perf } from "r3f-perf";
@@ -15,13 +17,25 @@ import * as THREE from "three";
  * Makes shadows look blurry by picking the shadow map texture at an offset
  * position according to the distance between the surface casting the shadow
  * and the surface receiving the shadow.
+ *
+ *
+ * Accumulative Shadows
+ *
+ * Accumulates multiple shadow renders where the light is moved randomly before
+ * each render. The shadow will be composed of multiple renders from various angles
+ * making it look soft and realistic.
+ * NOTE: Can only be rendered on a PLANE.
+ *       Make sure to remove any existing shadows on the plane.
+ *       Make sure to place it slightly above the plane to prevent z-fighting.
+ *       Requires its own lights!
  */
 
 export default function Experience() {
     const cube = useRef();
 
     const directionalLight = useRef();
-    useHelper(directionalLight, THREE.DirectionalLightHelper, 1);
+
+    // useHelper(directionalLight, THREE.DirectionalLightHelper, 1); // NOTE: Can cause artifacts when using with AccumulativeShadows
 
     useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2;
@@ -30,7 +44,7 @@ export default function Experience() {
     return (
         <>
             {/*<BakeShadows />*/}
-            <SoftShadows size={25} samples={17} focus={0.5} />
+            {/*<SoftShadows size={25} samples={17} focus={0.25} />*/}
 
             {/* Another way to add a background color. It must be attached for it to work. */}
             <color args={["ivory"]} attach="background" />
@@ -38,6 +52,25 @@ export default function Experience() {
             <Perf position="top-left" />
 
             <OrbitControls makeDefault />
+
+            <AccumulativeShadows
+                position={[0, -0.99, 0]}
+                scale={10}
+                color="#316d39"
+                opacity={0.8}
+                frames={Infinity}
+                temporal={true}
+                blend={100}
+            >
+                <RandomizedLight
+                    position={[1, 2, 3]}
+                    amount={8}
+                    radius={1}
+                    ambient={0.5}
+                    intensity={3}
+                    bias={0.001}
+                />
+            </AccumulativeShadows>
 
             <directionalLight
                 ref={directionalLight}
@@ -68,7 +101,7 @@ export default function Experience() {
                 position-y={-1}
                 rotation-x={-Math.PI * 0.5}
                 scale={10}
-                receiveShadow={true}
+                // receiveShadow={true}
             >
                 <planeGeometry />
                 <meshStandardMaterial color="greenyellow" />
