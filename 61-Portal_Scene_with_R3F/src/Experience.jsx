fbc5436d2afbea615dcda07d246c0308c8a47953
Christian Borja <cpborja32@gmail.com>
2024-07-30 15:41:26 -0700

    feat: add dark background color to scene

--- 61-Portal_Scene_with_R3F/src/Experience.jsx
+++ 61-Portal_Scene_with_R3F/src/Experience.jsx
@@ -3,6 +3,8 @@ import { OrbitControls } from "@react-three/drei";
 export default function Experience() {
     return (
         <>
+            <color args={["#030202"]} attach="background" />
+
             <OrbitControls makeDefault />
 
             <mesh scale={1.5}>
