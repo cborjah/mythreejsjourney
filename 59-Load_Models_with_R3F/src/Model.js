65d05540e2fab14dd862a7dd9711e1610e20bda9
Christian Borja <cpborja32@gmail.com>
2024-07-30 13:13:26 -0700

    feat: implement Clone to create multiple instances

--- 59-Load_Models_with_R3F/src/Model.js
+++ 59-Load_Models_with_R3F/src/Model.js
@@ -1,14 +1,23 @@
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, Clone } from "@react-three/drei";
 
 /**
  * NOTE: useGLTF from the Drei library will take care of everything.
  * No need to provide the DRACO decoder in the public folder!
+ *
+ * NOTE: Use 'Clone' to create multiple instances of an object while
+ * keeping the amount of geometries and shaders the same.
  */
 
 export default function Model() {
     const model = useGLTF("./hamburger-draco.glb");
 
-    return <primitive object={model.scene} scale={0.35} position-y={-1} />;
+    return (
+        <>
+            <Clone object={model.scene} scale={0.35} position-x={-4} />
+            <Clone object={model.scene} scale={0.35} position-x={0} />
+            <Clone object={model.scene} scale={0.35} position-x={4} />
+        </>
+    );
 }
 
 useGLTF.preload("./hamburger-draco.glb");
