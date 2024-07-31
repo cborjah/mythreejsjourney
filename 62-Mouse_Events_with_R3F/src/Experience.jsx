10cb96620cf5c8262846af17ab114bf5b5f81309
Christian Borja <cpborja32@gmail.com>
2024-07-31 16:22:00 -0700

    feat: stop event propagation on complex object (hamburger model)

--- 62-Mouse_Events_with_R3F/src/Experience.jsx
+++ 62-Mouse_Events_with_R3F/src/Experience.jsx
@@ -1,9 +1,10 @@
 import { useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import { useRef } from "react";
 
 export default function Experience() {
     const cube = useRef();
+    const hamburger = useGLTF("./hamburger.glb");
 
     useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2;
@@ -48,6 +49,18 @@ export default function Experience() {
                 <planeGeometry />
                 <meshStandardMaterial color="greenyellow" />
             </mesh>
+
+            <primitive
+                object={hamburger.scene}
+                scale={0.25}
+                position-y={0.5}
+                onClick={(event) => {
+                    // console.log(`clicked on ${event.object.name}!`); // Get exact mesh that triggered the event
+                    // console.log(event.eventObject); // Get object that triggered the event
+
+                    event.stopPropagation();
+                }}
+            />
         </>
     );
 }
