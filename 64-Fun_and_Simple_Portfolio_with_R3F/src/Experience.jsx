60d9c48cb58ef4f0d2b223f597faa410e7e72b55
Christian Borja <cpborja32@gmail.com>
2024-08-01 14:47:25 -0700

    feat: set PresentationControls to work from anywhere on the screen

--- 64-Fun_and_Simple_Portfolio_with_R3F/src/Experience.jsx
+++ 64-Fun_and_Simple_Portfolio_with_R3F/src/Experience.jsx
@@ -20,7 +20,7 @@ export default function Experience() {
 
             <color args={["#241a1a"]} attach="background" />
 
-            <PresentationControls>
+            <PresentationControls global>
                 <Float rotationIntensity={0.4}>
                     <primitive object={computer.scene} position-y={-1.2} />
                 </Float>
