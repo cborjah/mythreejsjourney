265fa2b05494a60d562adf83c7e65aea08d4b9e3
Christian Borja <cpborja32@gmail.com>
2024-08-01 15:02:36 -0700

    feat: tweak the physics of the model animations

--- 64-Fun_and_Simple_Portfolio_with_R3F/src/Experience.jsx
+++ 64-Fun_and_Simple_Portfolio_with_R3F/src/Experience.jsx
@@ -32,6 +32,8 @@ export default function Experience() {
                 rotation={[0.13, 0.1, 0]}
                 polar={[-0.4, 0.2]}
                 azimuth={[-1, 0.75]}
+                config={{ mass: 2, tension: 400 }}
+                snap={{ mass: 4, tension: 400 }}
             >
                 <Float rotationIntensity={0.4}>
                     <primitive object={computer.scene} position-y={-1.2} />
