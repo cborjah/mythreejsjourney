e59a848ef655006fc24cc701a0ec157ed6df92b9
Christian Borja <cpborja32@gmail.com>
2024-08-01 13:24:29 -0700

    feat: change color of custom effect

--- 63-Post_Processing_with_R3F/src/DrunkEffect.jsx
+++ 63-Post_Processing_with_R3F/src/DrunkEffect.jsx
@@ -9,7 +9,9 @@ import { Effect } from "postprocessing";
  */
 const fragmentShader = `
     void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
-        outputColor = vec4(uv, 1.0, 1.0);
+        vec4 color = inputColor;
+        color.rgb *= vec3(0.8, 1.0, 0.5);
+        outputColor = color;
     }
 `;
 
