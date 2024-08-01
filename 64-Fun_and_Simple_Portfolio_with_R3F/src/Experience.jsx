d1890505b87ff3ab665f7739bec80684ec5da75b
Christian Borja <cpborja32@gmail.com>
2024-08-01 14:37:18 -0700

    feat: add computer model to scene

--- 64-Fun_and_Simple_Portfolio_with_R3F/src/Experience.jsx
+++ 64-Fun_and_Simple_Portfolio_with_R3F/src/Experience.jsx
@@ -1,16 +1,21 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, useGLTF, OrbitControls } from "@react-three/drei";
+
+// const computer =
 
 export default function Experience() {
+    const computer = useGLTF(
+        "https://threejs-journey.com/resources/models/macbook_model.gltf"
+    );
+
     return (
         <>
+            <Environment preset="city" />
+
             <color args={["#241a1a"]} attach="background" />
 
             <OrbitControls makeDefault />
 
-            <mesh>
-                <boxGeometry />
-                <meshNormalMaterial />
-            </mesh>
+            <primitive object={computer.scene} />
         </>
     );
 }
