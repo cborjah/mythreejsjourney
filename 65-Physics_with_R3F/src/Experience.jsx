cac576c603ec402b776c735b06bb7014668728d8
Christian Borja <cpborja32@gmail.com>
2024-08-01 20:01:22 -0700

    feat: apply physics to cube and floor

--- 65-Physics_with_R3F/src/Experience.jsx
+++ 65-Physics_with_R3F/src/Experience.jsx
@@ -1,5 +1,6 @@
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
+import { Physics, RigidBody } from "@react-three/rapier";
 
 /**
  * Rapier
@@ -27,20 +28,26 @@ export default function Experience() {
             <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
             <ambientLight intensity={1.5} />
 
-            <mesh castShadow position={[-2, 2, 0]}>
-                <sphereGeometry />
-                <meshStandardMaterial color="orange" />
-            </mesh>
+            <Physics>
+                <RigidBody>
+                    <mesh castShadow position={[-2, 2, 0]}>
+                        <sphereGeometry />
+                        <meshStandardMaterial color="orange" />
+                    </mesh>
+                </RigidBody>
 
-            <mesh castShadow position={[2, 2, 0]}>
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
+                <mesh castShadow position={[2, 2, 0]}>
+                    <boxGeometry />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
 
-            <mesh receiveShadow position-y={-1.25}>
-                <boxGeometry args={[10, 0.5, 10]} />
-                <meshStandardMaterial color="greenyellow" />
-            </mesh>
+                <RigidBody type="fixed">
+                    <mesh receiveShadow position-y={-1.25}>
+                        <boxGeometry args={[10, 0.5, 10]} />
+                        <meshStandardMaterial color="greenyellow" />
+                    </mesh>
+                </RigidBody>
+            </Physics>
         </>
     );
 }
