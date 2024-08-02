5281895c25a27cd85fb1fbf91221e98d98143c71
Christian Borja <cpborja32@gmail.com>
2024-08-02 14:07:43 -0700

    feat: add rotating kinematic object to scene

--- 65-Physics_with_R3F/src/Experience.jsx
+++ 65-Physics_with_R3F/src/Experience.jsx
@@ -1,3 +1,4 @@
+import * as THREE from "three";
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import {
@@ -8,6 +9,7 @@ import {
     RigidBody
 } from "@react-three/rapier";
 import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 /**
  * Rapier
@@ -35,16 +37,35 @@ import { useRef } from "react";
  *
  *
  * NOTE: In order to set an object's mass, you need to use a custom collider.
+ *
+ * NOTE: For dynamic and fixed objects, don't change their position and rotation
+ *       values at run time. Their purpose is only to set the original position
+ *       and rotation before letting Rapier update the objects.
+ *       Apply forces to an object to move them!
+ *
+ *
+ * Kinematic objects will only move if a force is applied on them, not by other
+ * objects hitting them.
  */
 
 export default function Experience() {
     const cube = useRef();
+    const twister = useRef();
 
     const cubeJump = () => {
         const mass = cube.current.mass();
         cube.current.applyImpulse({ x: 0, y: 5 * mass, z: 0 });
         cube.current.applyTorqueImpulse({ x: 0, y: Math.random() - 0.5, z: 0 });
     };
+
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime();
+
+        const eulerRotation = new THREE.Euler(0, time * 3, 0);
+        const quaternionRotation = new THREE.Quaternion();
+        quaternionRotation.setFromEuler(eulerRotation);
+        twister.current.setNextKinematicRotation(quaternionRotation);
+    });
 
     return (
         <>
@@ -82,6 +103,18 @@ export default function Experience() {
                     <mesh receiveShadow position-y={-1.25}>
                         <boxGeometry args={[10, 0.5, 10]} />
                         <meshStandardMaterial color="greenyellow" />
+                    </mesh>
+                </RigidBody>
+
+                <RigidBody
+                    ref={twister}
+                    position={[0, -0.8, 0]}
+                    friction={0}
+                    type="kinematicPosition"
+                >
+                    <mesh castShadow scale={[0.4, 0.4, 3]}>
+                        <boxGeometry />
+                        <meshStandardMaterial color="red" />
                     </mesh>
                 </RigidBody>
             </Physics>
