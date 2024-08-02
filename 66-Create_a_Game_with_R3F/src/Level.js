c8e363ed9bd1b93df64fca2284960e6e1c0ea673
Christian Borja <cpborja32@gmail.com>
2024-08-02 15:51:23 -0700

    refactor: move geometries and material instances outside of component

--- 66-Create_a_Game_with_R3F/src/Level.js
+++ 66-Create_a_Game_with_R3F/src/Level.js
@@ -1,10 +1,21 @@
+import * as THREE from "three";
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
+const floor2Material = new THREE.MeshStandardMaterial({ color: "greenyellow" });
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "orangered" });
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
+
 function BlockStart({ position = [0, 0, 0] }) {
     return (
         <group position={position}>
-            <mesh position={[0, -0.1, 0]} receiveShadow>
-                <boxGeometry args={[4, 0.2, 4]} />
-                <meshStandardMaterial color="limegreen" />
-            </mesh>
+            <mesh
+                geometry={boxGeometry}
+                material={floor1Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
         </group>
     );
 }
