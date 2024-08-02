6edf2612b42dfb8cb1df7129b57738b8e748e31e
Christian Borja <cpborja32@gmail.com>
2024-08-01 19:43:26 -0700

    docs: add comments on the Rapier library

--- 65-Physics_with_R3F/src/Experience.jsx
+++ 65-Physics_with_R3F/src/Experience.jsx
@@ -1,31 +1,46 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
-export default function Experience()
-{
-    return <>
+/**
+ * Rapier
+ *
+ * Rapier is fully cross-platform deterministic.
+ *
+ * Running the simulation with the same INITIAL conditions, same version of Rapier,
+ * on two different machines (even with different browsers, operating systems, and
+ * processors, will result in the same results (by default).
+ * - https://rapier.rs/docs/user_guides/javascript/determinism/
+ *
+ *
+ * React Three Fiber already implements Rapier as React Three Rapier thanks to the
+ * PMDRS team!
+ *
+ */
 
-        <Perf position="top-left" />
+export default function Experience() {
+    return (
+        <>
+            <Perf position="top-left" />
 
-        <OrbitControls makeDefault />
+            <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+            <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+            <ambientLight intensity={1.5} />
 
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+            <mesh castShadow position={[-2, 2, 0]}>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
 
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <mesh castShadow position={[2, 2, 0]}>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
 
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+            <mesh receiveShadow position-y={-1.25}>
+                <boxGeometry args={[10, 0.5, 10]} />
+                <meshStandardMaterial color="greenyellow" />
+            </mesh>
+        </>
+    );
 }
