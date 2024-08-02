4fa74d497f1fd3afff075e059fa0e9357c42562d
Christian Borja <cpborja32@gmail.com>
2024-08-02 15:35:33 -0700

    refactor: extract meshes into Level file

--- 66-Create_a_Game_with_R3F/src/Experience.jsx
+++ 66-Create_a_Game_with_R3F/src/Experience.jsx
@@ -1,28 +1,14 @@
-import { OrbitControls } from '@react-three/drei'
-import Lights from './Lights.jsx'
+import { OrbitControls } from "@react-three/drei";
+import Lights from "./Lights.jsx";
+import Level from "./Level";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+    return (
+        <>
+            <OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
-
-        <Lights />
-
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+            <Lights />
+            <Level />
+        </>
+    );
 }
