a417e94d23ed2c16e1dea571f95e0230e02b67b9
Christian Borja <cpborja32@gmail.com>
2024-08-02 22:04:25 -0700

    feat: add limbo obstacle block

--- 66-Create_a_Game_with_R3F/src/Level.js
+++ 66-Create_a_Game_with_R3F/src/Level.js
@@ -65,11 +65,55 @@ function BlockSpinner({ position = [0, 0, 0] }) {
     );
 }
 
+function BlockLimbo({ position = [0, 0, 0] }) {
+    const obstacle = useRef();
+    const [timeOffset] = useState(() => Math.random() * Math.PI * 2);
+
+    useFrame((state) => {
+        const time = state.clock.elapsedTime;
+
+        const y = Math.sin(time + timeOffset) + 1.15;
+        obstacle.current.setNextKinematicTranslation({
+            x: position[0],
+            y: position[1] + y,
+            z: position[2]
+        });
+    });
+
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor2Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+            <RigidBody
+                ref={obstacle}
+                type="kinematicPosition"
+                position={[0, 0.3, 0]}
+                restitution={0.2}
+                friction={0}
+            >
+                <mesh
+                    geometry={boxGeometry}
+                    material={obstacleMaterial}
+                    scale={[3.5, 0.3, 0.3]}
+                    castShadow
+                    receiveShadow
+                />
+            </RigidBody>
+        </group>
+    );
+}
+
 export default function Level() {
     return (
         <>
-            <BlockStart position={[0, 0, 0]} />
+            <BlockStart position={[0, 0, 8]} />
             <BlockSpinner position={[0, 0, 4]} />
+            <BlockLimbo position={[0, 0, 0]} />
         </>
     );
 }
