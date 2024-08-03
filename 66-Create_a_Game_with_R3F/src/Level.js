e2954dbcdc252874ab01154713e6427811f982b0
Christian Borja <cpborja32@gmail.com>
2024-08-02 21:51:06 -0700

    feat: add rotation animation to obstacle

--- 66-Create_a_Game_with_R3F/src/Level.js
+++ 66-Create_a_Game_with_R3F/src/Level.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import { RigidBody } from "@react-three/rapier";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
@@ -21,6 +24,16 @@ function BlockStart({ position = [0, 0, 0] }) {
 }
 
 function BlockSpinner({ position = [0, 0, 0] }) {
+    const obstacle = useRef();
+
+    useFrame((state) => {
+        const time = state.clock.elapsedTime;
+
+        const rotation = new THREE.Quaternion();
+        rotation.setFromEuler(new THREE.Euler(0, time, 0));
+        obstacle.current.setNextKinematicRotation(rotation);
+    });
+
     return (
         <group position={position}>
             <mesh
@@ -30,13 +43,21 @@ function BlockSpinner({ position = [0, 0, 0] }) {
                 scale={[4, 0.2, 4]}
                 receiveShadow
             />
-            <mesh
-                geometry={boxGeometry}
-                material={obstacleMaterial}
-                scale={[3.5, 0.3, 0.3]}
-                castShadow
-                receiveShadow
-            />
+            <RigidBody
+                ref={obstacle}
+                type="kinematicPosition"
+                position={[0, 0.3, 0]}
+                restitution={0.2}
+                friction={0}
+            >
+                <mesh
+                    geometry={boxGeometry}
+                    material={obstacleMaterial}
+                    scale={[3.5, 0.3, 0.3]}
+                    castShadow
+                    receiveShadow
+                />
+            </RigidBody>
         </group>
     );
 }
