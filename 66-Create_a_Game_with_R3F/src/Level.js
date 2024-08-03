0475390eaba70ccb2b67021626ad7b22313ab134
Christian Borja <cpborja32@gmail.com>
2024-08-02 22:11:29 -0700

    feat: add axe obstacle block and ending block

--- 66-Create_a_Game_with_R3F/src/Level.js
+++ 66-Create_a_Game_with_R3F/src/Level.js
@@ -23,6 +23,20 @@ function BlockStart({ position = [0, 0, 0] }) {
     );
 }
 
+function BlockEnd({ position = [0, 0, 0] }) {
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor1Material}
+                position={[0, 0, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+        </group>
+    );
+}
+
 function BlockSpinner({ position = [0, 0, 0] }) {
     const obstacle = useRef();
     const [speed] = useState(
@@ -108,12 +122,57 @@ function BlockLimbo({ position = [0, 0, 0] }) {
     );
 }
 
+function BlockAxe({ position = [0, 0, 0] }) {
+    const obstacle = useRef();
+    const [timeOffset] = useState(() => Math.random() * Math.PI * 2);
+
+    useFrame((state) => {
+        const time = state.clock.elapsedTime;
+
+        const x = Math.sin(time + timeOffset) * 1.25;
+        obstacle.current.setNextKinematicTranslation({
+            x: position[0] + x,
+            y: position[1] + 0.75,
+            z: position[2]
+        });
+    });
+
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor2Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+            <RigidBody
+                ref={obstacle}
+                type="kinematicPosition"
+                position={[0, 0.3, 0]}
+                restitution={0.2}
+                friction={0}
+            >
+                <mesh
+                    geometry={boxGeometry}
+                    material={obstacleMaterial}
+                    scale={[1.5, 1.5, 0.3]}
+                    castShadow
+                    receiveShadow
+                />
+            </RigidBody>
+        </group>
+    );
+}
+
 export default function Level() {
     return (
         <>
-            <BlockStart position={[0, 0, 8]} />
-            <BlockSpinner position={[0, 0, 4]} />
-            <BlockLimbo position={[0, 0, 0]} />
+            <BlockStart position={[0, 0, 16]} />
+            <BlockSpinner position={[0, 0, 12]} />
+            <BlockLimbo position={[0, 0, 8]} />
+            <BlockAxe position={[0, 0, 4]} />
+            <BlockEnd position={[0, 0, 0]} />
         </>
     );
 }
