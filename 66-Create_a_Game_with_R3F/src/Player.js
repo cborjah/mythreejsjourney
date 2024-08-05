b9f032bfb59f5ce6424e1fff558ff53ac04a58b5
Christian Borja <cpborja32@gmail.com>
2024-08-05 15:04:06 -0700

    feat: configure rigid body for player

--- 66-Create_a_Game_with_R3F/src/Player.js
+++ 66-Create_a_Game_with_R3F/src/Player.js
@@ -1,8 +1,21 @@
 import { RigidBody } from "@react-three/rapier";
 
+/**
+ * NOTE: A RigidBody falls asleep after a few seconds of inaction.
+ *       This would result in the 'ball' not moving even though
+ *       the player presses the directional keys.
+ *       Set the 'canSleep' attribute to false to prevent this.
+ */
+
 export default function Player() {
     return (
-        <RigidBody position={[0, 1, 0]}>
+        <RigidBody
+            position={[0, 1, 0]}
+            colliders="ball"
+            restitution={0.2}
+            friction={1}
+            canSleep={false}
+        >
             <mesh castShadow>
                 <icosahedronGeometry args={[0.3, 1]} />
                 <meshStandardMaterial color="mediumpurple" flatShading />
