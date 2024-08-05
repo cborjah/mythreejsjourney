23b196400058de9dde71bc8ca035fd13c5af4b3f
Christian Borja <cpborja32@gmail.com>
2024-08-05 15:28:32 -0700

    feat: add ability to move 'ball' using keys

--- 66-Create_a_Game_with_R3F/src/Player.js
+++ 66-Create_a_Game_with_R3F/src/Player.js
@@ -1,4 +1,7 @@
+import { useRef } from "react";
 import { RigidBody } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+import { useKeyboardControls } from "@react-three/drei";
 
 /**
  * NOTE: A RigidBody falls asleep after a few seconds of inaction.
@@ -8,8 +11,51 @@ import { RigidBody } from "@react-three/rapier";
  */
 
 export default function Player() {
+    const body = useRef();
+    const [subscribeKeys, getKeys] = useKeyboardControls();
+
+    useFrame((state, delta) => {
+        const { forward, backward, leftward, rightward } = getKeys();
+
+        const impulse = { x: 0, y: 0, z: 0 };
+        const torque = { x: 0, y: 0, z: 0 };
+
+        /*
+         * NOTE: Before applying forces, you need to handle the variation in frame
+         * rates. If the same impulse is applied on a computer with a higher
+         * frame rate, the 'ball' will move much faster because applyImpulse
+         * will be called more often.
+         */
+        const impulseStrength = 0.6 * delta;
+        const torqueStrength = 0.2 * delta;
+
+        if (forward) {
+            impulse.z -= impulseStrength;
+            torque.x -= torqueStrength;
+        }
+
+        if (backward) {
+            impulse.z += impulseStrength;
+            torque.x += torqueStrength;
+        }
+
+        if (leftward) {
+            impulse.x -= impulseStrength;
+            torque.z += torqueStrength;
+        }
+
+        if (rightward) {
+            impulse.x += impulseStrength;
+            torque.z -= torqueStrength;
+        }
+
+        body.current.applyImpulse(impulse);
+        body.current.applyTorqueImpulse(torque);
+    });
+
     return (
         <RigidBody
+            ref={body}
             position={[0, 1, 0]}
             colliders="ball"
             restitution={0.2}
