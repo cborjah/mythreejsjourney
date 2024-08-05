720e20b71391eaf1f533008722cadca2a3cb03ef
Christian Borja <cpborja32@gmail.com>
2024-08-05 15:09:49 -0700

    feat: add KeyboardControls

--- 66-Create_a_Game_with_R3F/src/index.jsx
+++ 66-Create_a_Game_with_R3F/src/index.jsx
@@ -1,20 +1,44 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+import { KeyboardControls } from "@react-three/drei";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+/*
+ * KeyboardControls
+ *
+ * NOTE: <KeyboardControls /> needs to wrap EVERY component that has to be
+ *       aware of which keys are being pressed.
+ *
+ * The map attribute is an array that needs each key that needs to be
+ * observed.
+ */
 
 root.render(
-    <Canvas
-        shadows
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ 2.5, 4, 6 ]
-        } }
+    <KeyboardControls
+        map={[
+            {
+                name: "forward",
+                keys: ["ArrowUp", "KeyW"]
+            },
+            { name: "backward", keys: ["ArrowDown", "KeyS"] },
+            { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+            { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+            { name: "jump", keys: ["Space"] }
+        ]}
     >
-        <Experience />
-    </Canvas>
-)
+        <Canvas
+            shadows
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [2.5, 4, 6]
+            }}
+        >
+            <Experience />
+        </Canvas>
+    </KeyboardControls>
+);
