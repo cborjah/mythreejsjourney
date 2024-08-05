2092b6c065c79be34d3030f6a2d070178774afd3
Christian Borja <cpborja32@gmail.com>
2024-08-05 15:00:28 -0700

    feat: add rigid body to player mesh

--- 66-Create_a_Game_with_R3F/src/Player.js
+++ 66-Create_a_Game_with_R3F/src/Player.js
@@ -1,10 +1,12 @@
+import { RigidBody } from "@react-three/rapier";
+
 export default function Player() {
     return (
-        <>
+        <RigidBody position={[0, 1, 0]}>
             <mesh castShadow>
                 <icosahedronGeometry args={[0.3, 1]} />
                 <meshStandardMaterial color="mediumpurple" flatShading />
             </mesh>
-        </>
+        </RigidBody>
     );
 }
