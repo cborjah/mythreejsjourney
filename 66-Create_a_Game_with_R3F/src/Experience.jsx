aa096305e65db0bfd378caec48d8efd3a40fcdfe
Christian Borja <cpborja32@gmail.com>
2024-08-06 12:15:54 -0700

    refactor: remove OrbitControls

--- 66-Create_a_Game_with_R3F/src/Experience.jsx
+++ 66-Create_a_Game_with_R3F/src/Experience.jsx
@@ -1,4 +1,3 @@
-import { OrbitControls } from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
 import Lights from "./Lights.jsx";
 import { Level } from "./Level";
@@ -7,8 +6,6 @@ import Player from "./Player.js";
 export default function Experience() {
     return (
         <>
-            <OrbitControls makeDefault />
-
             <Physics debug={false}>
                 <Lights />
                 <Level />
