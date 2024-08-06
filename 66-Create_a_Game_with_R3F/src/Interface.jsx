2533eb36b0696700e09e8e95e2bbdd3374e96aa6
Christian Borja <cpborja32@gmail.com>
2024-08-06 13:07:07 -0700

    feat: add restart button

--- 66-Create_a_Game_with_R3F/src/Interface.jsx
+++ 66-Create_a_Game_with_R3F/src/Interface.jsx
@@ -2,6 +2,7 @@ export default function Interface() {
     return (
         <div className="interface">
             <div className="time">0.00</div>
+            <div className="restart">Restart</div>
         </div>
     );
 }
