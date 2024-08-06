ce2f246fd71e62ff96c4960cc06412575bb1362a
Christian Borja <cpborja32@gmail.com>
2024-08-06 16:16:25 -0700

    feat: end finish block text

--- 66-Create_a_Game_with_R3F/src/Level.js
+++ 66-Create_a_Game_with_R3F/src/Level.js
@@ -47,6 +47,14 @@ export function BlockEnd({ position = [0, 0, 0] }) {
 
     return (
         <group position={position}>
+            <Text
+                font="./bebas-neue-v9-latin-regular.woff"
+                scale={1}
+                position={[0, 2.25, 2]}
+            >
+                FINISH
+                <meshBasicMaterial toneMapped={false} />
+            </Text>
             <mesh
                 geometry={boxGeometry}
                 material={floor1Material}
