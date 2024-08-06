181d773d5e2abe091cdfc7290e39ffae7a2849df
Christian Borja <cpborja32@gmail.com>
2024-08-06 16:06:35 -0700

    feat: add background color

--- 66-Create_a_Game_with_R3F/src/Experience.jsx
+++ 66-Create_a_Game_with_R3F/src/Experience.jsx
@@ -14,6 +14,8 @@ export default function Experience() {
 
     return (
         <>
+            <color args={["#bdedfc"]} attach={"background"} />
+
             <Physics debug={false}>
                 <Lights />
                 <Level count={blocksCount} seed={blocksSeed} />
